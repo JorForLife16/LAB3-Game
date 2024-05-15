c3f931c447a0b4c0178bc8c6e7b791b05c1780cf
JorForLife16 <62538876+JorForLife16@users.noreply.github.com>
2024-05-15 22:45:42 +0700

    done

--- Debug/Middlewares/Third_Party/ARM_CMSIS/Source/BayesFunctions/BayesFunctions.d
+++ Debug/Middlewares/Third_Party/ARM_CMSIS/Source/BayesFunctions/BayesFunctions.d
@@ -1,6 +1,6 @@
 Middlewares/Third_Party/ARM_CMSIS/Source/BayesFunctions/BayesFunctions.o: \
  ../Middlewares/Third_Party/ARM_CMSIS/Source/BayesFunctions/BayesFunctions.c \
- C:/Users/user/Documents/GitHub/LAB03/Source/BayesFunctions/arm_gaussian_naive_bayes_predict_f32.c \
+ C:/Users/user/Documents/GitHub/LAB3-Game/Source/BayesFunctions/arm_gaussian_naive_bayes_predict_f32.c \
  ../Middlewares/Third_Party/ARM_CMSIS/Include/dsp/bayes_functions.h \
  ../Middlewares/Third_Party/ARM_CMSIS/Include/arm_math_types.h \
  ../Drivers/CMSIS/Include/cmsis_compiler.h \
@@ -12,7 +12,7 @@ Middlewares/Third_Party/ARM_CMSIS/Source/BayesFunctions/BayesFunctions.o: \
  ../Middlewares/Third_Party/ARM_CMSIS/Include/dsp/statistics_functions.h \
  ../Middlewares/Third_Party/ARM_CMSIS/Include/dsp/basic_math_functions.h \
  ../Middlewares/Third_Party/ARM_CMSIS/Include/dsp/fast_math_functions.h
-C:/Users/user/Documents/GitHub/LAB03/Source/BayesFunctions/arm_gaussian_naive_bayes_predict_f32.c:
+C:/Users/user/Documents/GitHub/LAB3-Game/Source/BayesFunctions/arm_gaussian_naive_bayes_predict_f32.c:
 ../Middlewares/Third_Party/ARM_CMSIS/Include/dsp/bayes_functions.h:
 ../Middlewares/Third_Party/ARM_CMSIS/Include/arm_math_types.h:
 ../Drivers/CMSIS/Include/cmsis_compiler.h:
